89c599bd06898bc76046f22bf616374bb85289e2
Ritesh Kumar <riteshkumar12040211@gmail.com>
2026-03-21 13:08:05 +0530

    fix: update API baseURL

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-baseURL:"https://team-collab-api-2.onrender.com/api"})
+baseURL:"https://team-collab-api-btlh.onrender.com/api"})
 
 // attach token automatically
 API.interceptors.request.use((req) => {
